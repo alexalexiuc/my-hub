300ff0087efbf9d66fd0234f831362339d2ed646
Alex Alexiuc <27913426+alexalexiuc@users.noreply.github.com>
2026-04-06 11:26:34 +0300

    chore: fix failing e2e test stup

--- packages/e2e/tsup.config.ts
+++ packages/e2e/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: { 'setup-e2e-db': 'scripts/setup-e2e-db.ts' },
   format: ['esm'],
   target: 'node22',
+  platform: 'node',
   bundle: true,
   noExternal: [/./],
   clean: true,
