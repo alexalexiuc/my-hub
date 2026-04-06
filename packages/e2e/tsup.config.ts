aa800156b2452842825336d7c658e83c31f27942
Alex Alexiuc <27913426+alexalexiuc@users.noreply.github.com>
2026-04-06 15:22:53 +0300

    fix: Fix e2e seeds build

--- packages/e2e/tsup.config.ts
+++ packages/e2e/tsup.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: { 'setup-e2e-db': 'scripts/setup-e2e-db.ts' },
   format: ['esm'],
-  target: 'node22',
-  platform: 'node',
-  bundle: true,
-  noExternal: [/./],
   clean: true,
   sourcemap: false,
 });
