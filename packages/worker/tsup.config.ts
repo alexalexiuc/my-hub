d4800a2a432ba5cfc5ec2032531492f9ce24173f
Alex Alexiuc <27913426+alexalexiuc@users.noreply.github.com>
2026-04-15 09:18:37 +0300

    chore: Add run-task entry point in worker

--- packages/worker/tsup.config.ts
+++ packages/worker/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: { index: 'src/index.ts' },
+  entry: { index: 'src/index.ts', 'run-task': 'scripts/run-task.ts' },
   format: ['esm'],
   target: 'node22',
   clean: true,
